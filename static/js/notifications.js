7e29fd14968a320fdbf96d43d03cdebd8db70fdc
lKeoNl <karbovsky2006@gmail.com>
2025-07-02 11:03:25 +0300

    notifications fix

--- static/js/notifications.js
+++ static/js/notifications.js
@@ -1,12 +1,18 @@
-function showNotification() {
+function showNotification(message = '') {
   const notification = document.querySelector('.notification');
   if (notification) {
+    notification.textContent = message;
+    notification.style.display = 'block';
+
     setTimeout(() => {
       notification.classList.add('show');
     }, 100);
 
     setTimeout(() => {
       notification.classList.remove('show');
+      setTimeout(() => {
+        notification.style.display = 'none';
+      }, 400);
     }, 4000);
   }
 }
